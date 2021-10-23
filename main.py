e259fdc8d092db1742494f7b577d498df5811f2f
root <root@localhost>
2021-10-23 15:10:10 +0100

    auto publish 1634998207999

---
+++ main.py
@@ -0,0 +1,73 @@
+likes = 43
+likes = 71
+likes = 28
+print (likes)
+families = 26
+families = 71
+famiies  = 10
+print (families)
+name =("precious")
+country =("Nigeria")
+letter =("p")
+dream =("I want to be a programmer")
+school =("University of Benin")
+Isrich =True
+Isscared =False
+Isithottoday =True
+Isitrainy =True
+input("what is my best car")
+name =("Rolls Royce")
+print ("Rolls Royce" + name)
+input("who is your best friend")
+name =("David")
+print ("David" + name)
+siblings = input (siblings_text)
+print (siblings)
+Ironman = 5
+Spiderman = 4
+total = Ironman + Spiderman
+print (9)
+boys = 6
+girls = 16
+total = boys + girls
+print (22)
+price = 70
+if price < 500:
+  print ("i will buy bananas")
+else :
+  print ("i will not buy")
+price = 80
+if price < 100:
+  print ("i will buy the clothes")
+else :
+  print ("i will not buy")
+price = 50
+if price < 10:
+  print ("i will go to school")
+  print ("i will not go to school")
+count = 12
+if count == 12:
+  print ("i will stay")
+else :
+  print ("i will go somewhere else")
+name = ("Precious")
+if name == "Precious":
+  print ("Precious is my name")
+else :
+  print ("It is not my name")
+salary = 24
+Vacation_days = 35
+if salary > 15 and vacation_days > 20:
+  print ("i will be happy")
+else :
+  print ("i will leave")
+cars = 15000
+clothes = 10000
+if cars < 15000 and clothes < 10000:
+  print ("i will buy")
+else :
+  print ("i will go somewhere else")
+
+
+  
+
